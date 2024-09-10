1f6b09331acf4bc002589ef440706a58db019adf
Shikory <shikory1@gmail.com>
2024-09-10 12:12:29 -0600

    Modificaciones al codigo para mantener continuidad en las grabaciones sin pausas.

--- mejora2au.py
+++ mejora2au.py
@@ -1,3 +1,7 @@
+# El código que proporcionó es un script de Python que incluye múltiples importaciones para diferentes
+# bibliotecas/módulos, inicialización de variables globales y definiciones de funciones para grabar audio,
+# transcribir el discurso y administrar el proceso de transcripción.
+
 import threading  # para usar varios hilos al mismo tiempo
 import sys        # para salir del programa
 import datetime   # usar hora y fecha
@@ -18,11 +22,15 @@ n = 1  # número de la grabaciones REC A
 l = 2  # número de la grabaciones REC B
 m = 1  # número de fragmento analizado con whisper
 total = 0  # número total de grabaciones
-chico = 16  # tiempo de espera para obtener el primer archivo de audio .wav
-infinity = 0  # variable para
+chico = 20  # tiempo de espera para obtener el primer archivo de audio .wav
+infinity = 0  # variable para terminar programa
 
 
 def REC_A():    # GRABACIÓN #####################################################
+    """
+    La función `Rec_A` registra la entrada de audio durante 15 segundos, la guarda en un archivo WAV e incrementa el
+Número de grabación por 2.
+    """
 
     global n  # comando para utlizar la variable global n desiganda para las grabaciones A
 
@@ -80,17 +88,23 @@ def REC_A():    # GRABACIÓN ###################################################
     obj.writeframes(b"".join(frames))
     obj.close
     print(f"Archivo outputA_{n}.wav generado.")
-    n += 2
-    if keyboard.is_pressed('q'):
+    
+    
+    n += 2              # Incrementa en 2 el valor de n
+    
+    if keyboard.is_pressed('q'):    # si se presiona la letra q deja de grabar y llama a la funcion Whis_Infi()
         print("Dejando de grabar, se procede a solo transcribir..."+"\n")
         Whis_Infi()
-
+        
+    #time.sleep(chico)
+    h3.start()
 
 def REC_B():    # GRABACIÓN #####################################################
 
     global l  # comando para utlizar la variable global n desiganda para las grabaciones B
 
-    time.sleep(10)
+    time.sleep(10) # espera 10 segundos antes de grabar
+    
     # CONFIGURACIÓN DEL ARCHIVO DE GRABACIÓN
     FRAMES_PER_BUFFER = 3200
     FORMAT = pyaudio.paInt16
@@ -145,7 +159,9 @@ def REC_B():    # GRABACIÓN ###################################################
     obj.writeframes(b"".join(frames))
     obj.close
     print(f"Archivo outputA_{l}.wav generado.")
+    
     l += 2
+    
     if keyboard.is_pressed('q'):
         print("Dejando de grabar, se procede a solo transcribir..."+"\n")
         Whis_Infi()
@@ -153,11 +169,14 @@ def REC_B():    # GRABACIÓN ###################################################
 
 def Whis():  # WHISPER ##############################################################
     
+    h1.join()
+    h2.join()
     global chico  # la variable global chico desiganda para esperar los fragmentos de las grabaciones A
     global m  # la variable global m desiganda para analizar los fragmentos de las grabaciones A
-
+    chico = 0
+    
     if os.path.exists(file_path):
-        
+
         fechaActual = datetime.datetime.now()
         fechaStrActual = datetime.datetime.strftime(
         fechaActual, '%d/%m/%Y %H:%M:%S')
@@ -165,7 +184,8 @@ def Whis():  # WHISPER #########################################################
         inicio = datetime.datetime.now()
 
         print(f"Analizando audio output{m}.wav")
-        # SELECCIÓN DE MODELO small PARA RAPIDO, medium PARA PRECISIÓN, large PARA MAXIMA PRECISIÖN
+        
+        # CONFIGURACIÓN DEL MODULO WHISPER 'small' PARA RAPIDO, medium PARA PRECISIÓN, large PARA MAXIMA PRECISIÓN
         model = whisper.load_model('small')
         Texto = model.transcribe(f"outputA{m}.wav", language='Spanish', fp16=False)
 
@@ -176,27 +196,42 @@ def Whis():  # WHISPER #########################################################
         print(f'Transcipción finalizada en {Tiempo.seconds} segundos'+"\n")
         print("''" + Texto["text"] + "''"+"\n")
 
+        # Almacenado de texto en archivo Texto_captura2.txt
+        
         with open(nombre_archivo, '+a') as archivo:
             archivo.write(fechaStrActual + f" Audio#{m} " + Texto["text"]+"\n")
         print("Texto agregado a Texto_captura2.txt")
+        
         m += 1
+        
+        print(f"n = {n}"+"\n")
+        print(f"l = {l}"+"\n")
+        print(f"total = {total}"+"\n")
+        print(f"m = {m}"+"\n")
+        
     else:
         print('El archivo no existe')
+        
 
 
 def Whis_Infi():  # Blucle hasta terminar traduccion de todos los audios.#################################################
 
+    h1.join()
+    h2.join()
+    h3.join()
+    
     global m  # la variable global m desiganda para analizar los fragmentos de las grabaciones A
-    global n  # la variable global n desiganda para analizar los fragmentos de las grabaciones A
-    global l  # la variable global l desiganda para analizar los fragmentos de las grabaciones B
+    #global n  # la variable global n desiganda para analizar los fragmentos de las grabaciones A
+    #global l  # la variable global l desiganda para analizar los fragmentos de las grabaciones B
     global total  # la variable global total desiganda para contabilizar todas las grabaciones
-
-    if n > m:
+    global infinity  # la variable global infiniti para terminar el programa
+    
+    if n > l:
         total = n
     else:
-        total = m
+        total = l
 
-    while m <= total:
+    while m <= total:       # MIENTRAS 'm' SEA MENOR O IGUAL A 'total' SE TRANSCRIBIRAN LOS AUDIOS HASTA TERMINAR
 
         fechaActual = datetime.datetime.now()
         fechaStrActual = datetime.datetime.strftime(
@@ -218,12 +253,18 @@ def Whis_Infi():  # Blucle hasta terminar traduccion de todos los audios.#######
         with open(nombre_archivo, '+a') as archivo:
             archivo.write(fechaStrActual + f" Audio#{m} " + Texto["text"]+"\n")
         print("Texto agregado a Texto_captura2.txt")
+        
+        print(f"n = {n}"+"\n")
+        print(f"l = {l}"+"\n")
+        print(f"total = {total}"+"\n")
+        print(f"m = {m}"+"\n")
+              
         m += 1
-        if m == total:
-            print("Transcripción COMPLETADA en Texto_captura2.txt")
-            print("Presione una tecla para continuar...")
-            exit()
-
+        
+    if m > total:
+        print("Transcripción COMPLETADA en Texto_captura2.txt")
+        infinity = 1
+            
 
 # MAIN ###################principal############################################
 if __name__ == '__main__':
@@ -232,7 +273,7 @@ if __name__ == '__main__':
     # Whis()
     
 
-    while True:
+    while infinity == 0:
         
         h1 = threading.Thread(name="REC_A", target=REC_A)
         h2 = threading.Thread(name="REC_B", target=REC_B)
@@ -240,11 +281,11 @@ if __name__ == '__main__':
 
         h1.start()
         h2.start()
-        h3.start()
+        #h3.start()
 
-        h1.join()
-        h2.join()
-        h3.join()
+        #h1.join()
+        #h2.join()
+        #h3.join()
 
         
 
@@ -253,5 +294,5 @@ if __name__ == '__main__':
         # if keyboard.is_pressed('q'):
         # break
 
-    # print("Presione una tecla para continuar...")
-    # msvcrt.getch()
+    print("Presione una tecla para continuar...")
+    msvcrt.getch()
